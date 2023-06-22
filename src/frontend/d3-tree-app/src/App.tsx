7ce3c08d7a6fefb9d9b4f0b0cf0015e6209d7c1f
runfourestrun <90913666+runfourestrun@users.noreply.github.com>
2023-06-22 12:42:20 -0700

    trying to get the d3 sankey diagram to work

--- src/frontend/d3-tree-app/src/App.tsx
+++ src/frontend/d3-tree-app/src/App.tsx
@@ -1,46 +1,41 @@
 import React, { useState } from 'react';
-import { useQuery,QueryClient,QueryClientProvider } from 'react-query'
+import { useQuery, QueryClient, QueryClientProvider } from 'react-query';
 import { gql } from '@apollo/client';
-import client from './client'
-
+import client from './client';
+import SankeyDiagram from './SankeyDiagram';
 
 interface Brand {
     brandName: string;
+    subBrands?: Brand[];
 }
 
-const GET_ALL_BRANDS = gql`
-    query GetAllBrands{
-        getAllBrands {
+const GET_SUB_BRANDS = gql`
+    query GetSubBrands($brandName: String!) {
+        getSubBrands(brandName: $brandName) {
             brandName
+            subBrands {
+                brandName
+            }
         }
     }
 `;
 
-
 const queryClient = new QueryClient();
 
-
 function App() {
-
-
-    //Hook to manage state for these two variables
     const [inputValue, setInputValue] = useState('');
 
-    //I think this is an event handler? It changes the state of event.target.value clearly though.
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.target.value);
     };
 
-
-
     const { isLoading, isError, data } = useQuery('brands', async () => {
         const response = await client.query({
-            query: GET_ALL_BRANDS,
+            query: GET_SUB_BRANDS,
             variables: { brandName: inputValue },
         });
         return response.data;
     });
-
 
     if (isLoading || data === undefined) {
         return <div>Loading...</div>;
@@ -50,18 +45,48 @@ function App() {
         return <div>Error fetching data</div>;
     }
 
+    const transformData = (response: any) => {
+        const { getSubBrands } = response;
 
-    const transformData = (data: Brand[]) => {
-        // Implement the logic to transform the GraphQL response data into the required format for the D3 Tree diagram
-        // Return the transformed data
+        const nodes: any[] = [];
+        const links: any[] = [];
+
+        const findOrCreateNode = (nodeName: string) => {
+            const existingNode = nodes.find((node) => node.name === nodeName);
+            if (existingNode) {
+                return existingNode;
+            } else {
+                const newNode = { name: nodeName };
+                nodes.push(newNode);
+                return newNode;
+            }
+        };
+
+        getSubBrands.forEach((brand: Brand) => {
+            const root = findOrCreateNode(brand.brandName);
+
+            if (brand.subBrands && brand.subBrands.length > 0) {
+                brand.subBrands.forEach((subBrand: Brand) => {
+                    const leaf = findOrCreateNode(subBrand.brandName);
+
+                    links.push({
+                        source: root.name,
+                        target: leaf.name,
+                        value: 1,
+                    });
+                });
+            }
+        });
+
+        return { nodes, links };
     };
+
+    const transformedData = transformData(data);
 
     return (
         <div className="App">
             <header className="App-header">
-                <input type="text" value={inputValue} onChange={handleInputChange} />
-                {/* Display the retrieved data */}
-                <pre>{JSON.stringify(data.getAllBrands, null, 2)}</pre>
+                <SankeyDiagram data={transformedData} width={500} height={500} />
             </header>
         </div>
     );
@@ -74,7 +99,5 @@ function Root() {
         </QueryClientProvider>
     );
 }
-
-
 
 export default Root;
